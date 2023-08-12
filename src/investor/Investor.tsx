d58fa1bb69c0d44c3245fc14a086ab491ffc277e
h3rt <alexander.hertlein@icloud.com>
2023-08-12 11:32:01 +0200

    Add NFT tile with info

--- src/investor/Investor.tsx
+++ src/investor/Investor.tsx
@@ -7,6 +7,10 @@ import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
 import { useEffect, useState, useContext } from "react";
 import { MetamaskContext } from "shared/context/MetamaskContext";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import NftTile from "./components/NftTile"
+import { ContractAddress } from "shared/utils/config";
 
 function Investor() {
   const {
@@ -15,6 +19,31 @@ function Investor() {
     disconnectWallet,
     connectWallet
   } = useContext(MetamaskContext);
+
+    // Search
+  const [searchText, setSearchText] = useState<string>("");
+
+  const [nftData, setNftData] = useState<any>("");
+  const [filteredNftData, setFilteredNftData] = useState<any>("");
+
+  interface INftItemProps {
+    name: string;
+    description: string;
+    image?: string;
+    tags?: string[];
+    url?: string;
+    TVL: number;
+    APR: number; 
+    Capacity: number;
+    tokenPrice: number;
+  }
+
+  useEffect(() => {
+    const data = {name: "Test", description: "Test", image: "", tags:"", url:"https://test", TVL: 100000, APR: 0.001, Capacity:10, tokenPrice: "0.01", tokenDenom:"USDC"}
+
+    setNftData([data])
+  }, []);
+
   return (
     <>
         <div className="max-w-2xl mx-auto px-6 text-neutral-600 dark:text-neutral-400 leading-7 text-justify">
@@ -24,6 +53,92 @@ function Investor() {
             Investor
           </h1>
         </div>
+        <div className="relative w-full sm:w-96 mx-auto mb-4">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <FontAwesomeIcon icon={faMagnifyingGlass} className="" />
+          </div>
+          <input
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            type="text"
+            id="search"
+            className="block w-full p-4 pl-10 text-sm rounded-lg text-neutral-800 dark:text-white bg-white dark:bg-neutral-800 placeholder-neutral-600 dark:placeholder-neutral-400 border border-neutral-300 dark:border-neutral-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-cyan-500 dark:focus-visible:ring-cyan-500"
+            placeholder="Search"
+          />
+        </div>
+        <div className="grid grid-cols-12 gap-4 auto-rows-auto">
+          {nftData?.length > 0 && (
+            <>
+              {nftData.map((NFT: any) => (console.log(NFT),
+                <NftTile
+                  key={NFT.name}
+                  name={NFT.name}
+                  description={NFT.description}
+                  image={""}
+                  url={NFT.url}
+                  TVL={NFT.TVL}
+                  APR={NFT.APR}
+                  Capacity={NFT.Capacity}
+                  tokenPrice={NFT.tokenPrice}
+                  tokenDenom={NFT.tokenDenom}
+                />
+              ))}
+            </>
+          )}
+
+          {nftData?.length <= 0 && (
+            <>
+              {/* Skeleton Loader Item */}
+              <div className="animate-pulse col-span-12 sm:col-span-6 lg:col-span-6 xl:col-span-4 2xl:col-span-3">
+                <div className="h-72 bg-white dark:bg-neutral-800 rounded-xl"></div>
+              </div>
+              {/* Skeleton Loader Item */}
+              <div className="animate-pulse col-span-12 sm:col-span-6 lg:col-span-6 xl:col-span-4 2xl:col-span-3">
+                <div className="h-72 bg-white dark:bg-neutral-800 rounded-xl"></div>
+              </div>
+              {/* Skeleton Loader Item */}
+              <div className="animate-pulse col-span-12 sm:col-span-6 lg:col-span-6 xl:col-span-4 2xl:col-span-3">
+                <div className="h-72 bg-white dark:bg-neutral-800 rounded-xl"></div>
+              </div>
+              {/* Skeleton Loader Item */}
+              <div className="animate-pulse col-span-12 sm:col-span-6 lg:col-span-6 xl:col-span-4 2xl:col-span-3">
+                <div className="h-72 bg-white dark:bg-neutral-800 rounded-xl"></div>
+              </div>
+              {/* Skeleton Loader Item */}
+              <div className="animate-pulse col-span-12 sm:col-span-6 lg:col-span-6 xl:col-span-4 2xl:col-span-3">
+                <div className="h-72 bg-white dark:bg-neutral-800 rounded-xl"></div>
+              </div>
+              {/* Skeleton Loader Item */}
+              <div className="animate-pulse col-span-12 sm:col-span-6 lg:col-span-6 xl:col-span-4 2xl:col-span-3">
+                <div className="h-72 bg-white dark:bg-neutral-800 rounded-xl"></div>
+              </div>
+              {/* Skeleton Loader Item */}
+              <div className="animate-pulse col-span-12 sm:col-span-6 lg:col-span-6 xl:col-span-4 2xl:col-span-3">
+                <div className="h-72 bg-white dark:bg-neutral-800 rounded-xl"></div>
+              </div>
+              {/* Skeleton Loader Item */}
+              <div className="animate-pulse col-span-12 sm:col-span-6 lg:col-span-6 xl:col-span-4 2xl:col-span-3">
+                <div className="h-72 bg-white dark:bg-neutral-800 rounded-xl"></div>
+              </div>
+              {/* Skeleton Loader Item */}
+              <div className="animate-pulse col-span-12 sm:col-span-6 lg:col-span-6 xl:col-span-4 2xl:col-span-3">
+                <div className="h-72 bg-white dark:bg-neutral-800 rounded-xl"></div>
+              </div>
+              {/* Skeleton Loader Item */}
+              <div className="animate-pulse col-span-12 sm:col-span-6 lg:col-span-6 xl:col-span-4 2xl:col-span-3">
+                <div className="h-72 bg-white dark:bg-neutral-800 rounded-xl"></div>
+              </div>
+              {/* Skeleton Loader Item */}
+              <div className="animate-pulse col-span-12 sm:col-span-6 lg:col-span-6 xl:col-span-4 2xl:col-span-3">
+                <div className="h-72 bg-white dark:bg-neutral-800 rounded-xl"></div>
+              </div>
+              {/* Skeleton Loader Item */}
+              <div className="animate-pulse col-span-12 sm:col-span-6 lg:col-span-6 xl:col-span-4 2xl:col-span-3">
+                <div className="h-72 bg-white dark:bg-neutral-800 rounded-xl"></div>
+              </div>
+            </>
+          )}
+         </div>
         </div>
     </>
   )
---
+++ src/investor/components/NftTile.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { ContractAddress } from "shared/utils/config";
+
+interface INftItemProps {
+  name: string;
+  description: string;
+  image?: string;
+  tags?: string[];
+  url?: string;
+  TVL: number;
+  APR: number; 
+  Capacity: number;
+  tokenPrice: number;
+  tokenDenom: string;
+}
+
+const NftTile = (props: INftItemProps) => {
+  const handleClick = () => {
+  };
+
+  return (
+    <a
+      href={props.url || "#"}
+      target={props.url ? "_blank" : "_self"}
+      onClick={handleClick}
+      className="group col-span-12 sm:col-span-6 lg:col-span-6 xl:col-span-4 2xl:col-span-3 hover:bg-gradient-to-r from-cyan-500 to-purple-500 p-0.5 rounded-xl hover:shadow-2xl hover:shadow-cyan-500/40"
+    >
+      <div className="bg-white group-hover:bg-white/95 dark:bg-neutral-800 group-hover:dark:bg-neutral-800/95 border border-neutral-200 dark:border-neutral-700 p-4 flex flex-col h-full rounded-xl overflow-hidden text-center sm:text-left">
+        {/* Image */}
+        {props.image && (
+          <img
+            src={props.image}
+            alt={`${props.name} logo`}
+            className="w-16 h-16 rounded-xl block mb-4 bg-neutral-100 dark:bg-neutral-900 flex-initial mx-auto sm:mx-0"
+          />
+        )}
+
+        {/* Name */}
+        <div className="text-xl font-semibold flex-initial mb-1">
+          {props.name}
+        </div>
+
+        {/* Description */}
+        <div className="text-neutral-400 flex-1">{props.description}</div>
+
+        {/* TVL */}
+        <div className="text-neutral-400 flex-1">{`TVL: ${props.TVL}`}</div>
+
+        {/* APR */}
+        <div className="text-neutral-400 flex-1">{`APR: ${props.APR}`}</div>
+
+        {/* Capacity */}
+        <div className="text-neutral-400 flex-1">{`Capacity: ${props.Capacity}`}</div>
+
+        {/* Token Price */}
+        <div className="text-neutral-400 flex-1">{`Token Price: ${props.tokenPrice}`}</div>
+
+        {/* Tags */}
+        {props.tags?.length! > 0 && (
+          <div className="space-x-2 mt-4 flex-initial">
+          </div>
+        )}
+      </div>
+    </a>
+  );
+};
+
+export default NftTile;
